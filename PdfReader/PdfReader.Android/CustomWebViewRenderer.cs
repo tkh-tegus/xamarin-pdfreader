19701d6ea1f6956901040fce514ff86b360a5263
TEGUIA Hervé <tkh.tegus@gmail.com>
2021-05-19 12:03:15 +0100

    init

---
+++ PdfReader/PdfReader.Android/CustomWebViewRenderer.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PdfReader;
+using PdfReader.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace PdfReader.Droid
+{
+    public class CustomWebViewRenderer : WebViewRenderer
+    {
+        public CustomWebViewRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var customWebView = Element as CustomWebView;
+                Control.Settings.AllowUniversalAccessFromFileURLs = true;
+                Control.LoadUrl(string.Format("file:///android_asset/pdfjs/web/viewer.html?file={0}", string.Format("file:///android_asset/Content/{0}", WebUtility.UrlEncode(customWebView.Uri))));
+            }
+        }
+    }
+}
---
+++ PdfReader/PdfReader/LoadPdf.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PdfReader
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoadPdf : ContentPage
+    {
+        public LoadPdf()
+        {
+            InitializeComponent();
+        }
+    }
+}
